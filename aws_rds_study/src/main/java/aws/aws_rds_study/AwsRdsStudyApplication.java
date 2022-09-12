f98960a5939ec4c20168da6e73b570a5a5ddafbc
Min Ho CHO <chominho96@naver.com>
2022-09-12 21:27:41 +0900

    1. 프로젝트 생성

---
+++ aws_rds_study/src/main/java/aws/aws_rds_study/AwsRdsStudyApplication.java
@@ -0,0 +1,13 @@
+package aws.aws_rds_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsRdsStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsRdsStudyApplication.class, args);
+	}
+
+}
---
+++ aws_rds_study/src/test/java/aws/aws_rds_study/AwsRdsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package aws.aws_rds_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsRdsStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
